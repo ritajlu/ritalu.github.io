c45f7fe70bda97093d86464500b9d5020573829d
reetah <rita.lu410@gmail.com>
2014-09-08 19:25:07 -0400

    added links to cv, linkedin, github

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 function aboutme() {
-    //console.log("ABOUTME"); 
+    // hide projects, show about me
     $('#projects_main').hide();
     $('#aboutme_main').show();
+
+    // underline about me, un-underline projects
+    $('#about').css('text-decoration','underline');
+    $('#projects').css('text-decoration','none');
+    $('#cv').css('text-decoration','none');
 }
 
 
 function projects(){
-	//console.log("PROJECTS");
     $('#aboutme_main').hide();
     $('#projects_main').show();
+
+    $('#projects').css('text-decoration','underline');
+    $('#about').css('text-decoration','none');
+    $('#cv').css('text-decoration','none');
+}
+
+function cv(){
+	$('#cv').css('text-decoration','underline');
+	$('#projects').css('text-decoration','none');
+    $('#about').css('text-decoration','none');
 }
