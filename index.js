96a5e17a2f98f408202e71c44c6ce94376d257d3
reetah <rita.lu410@gmail.com>
2014-09-08 15:12:25 -0400

    added tabs

---
+++ index.js
@@ -0,0 +1,12 @@
+function aboutme() {
+    //console.log("ABOUTME"); 
+    $('#projects_main').hide();
+    $('#aboutme_main').show();
+}
+
+
+function projects(){
+	//console.log("PROJECTS");
+    $('#aboutme_main').hide();
+    $('#projects_main').show();
+}
